d683c5218a05a76339a9958490ba5fff51ec40a8
Joao <JoaoVitorLeite>
2020-05-27 15:21:51 -0300

    27/05 - 15:21

--- src/main/scala/password/Main.scala
+++
@@ -1,9 +0,0 @@
-package password
-
-object Main {
-
-  def main(args: Array[String]): Unit = {
-    println(new PassGenerator.Gen().generate(8,upperCase = true,lowerCase = true,numbers = true,symbols = false))
-  }
-
-}
