52fb124119d1101f97c6daec5941da0af6c07870
Joao <JoaoVitorLeite>
2020-05-28 23:17:05 -0300

    28/05 - 23:16

--- src/main/scala/password/PassGenerator.scala
+++ src/main/scala/password/PassGenerator.scala
@@ -2,10 +2,36 @@ package password
 
 import scala.util.Random
 
+/***
+ * @author João Vitor Leite
+ */
+
+
+/***
+ * Object to generate random password
+ */
 object PassGenerator {
 
+  /***
+   * Implicit class to generate random password
+   *
+   * <p>
+   *   Note: If you want symbols in the password pass the symbols as a String to the Gen method
+   * </p>
+   *
+   * @param addsymbols Symbol String
+   */
   implicit class Gen(addsymbols: String = ""){
 
+    /***
+     * Method to define which elements the password will have
+     * @param size Password size
+     * @param upperCase Boolean parameter to define if there will be UpperCase in the password
+     * @param lowerCase Boolean parameter to define if there will be LowerCase in the password
+     * @param numbers Boolean parameter to define if there will be Numbers in the password
+     * @param symbols Boolean parameter to define if there will be Symbols in the password
+     * @return Random password in String format
+     */
     def generate(size: Int, upperCase: Boolean, lowerCase: Boolean, numbers: Boolean, symbols: Boolean): String ={
       val upper = (65 to 90).toList.map(_.toChar.toString)
       val lower = (97 to 122).toList.map(_.toChar.toString)
--- src/test/scala/password/PassGenSuite.scala
+++ src/test/scala/password/PassGenSuite.scala
@@ -4,64 +4,70 @@ import org.scalatest.FunSuite
 
 class PassGenSuite extends FunSuite{
 
+  /***
+   * <p>
+   *   Note: Because of the random password generation mechanism the tests have no stability in the result, that is, if you select a password with numbers and letters one of the possible results is a password with letters only
+   * </p>
+   */
+
   test("T1"){
-    val a = new PassGenerator.Gen().generate(20,true, false, false, false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = true, lowerCase = false, numbers = false, symbols = false)
     assert(a.exists(_.isUpper))
   }
   test("T2"){
-    val a = new PassGenerator.Gen().generate(20,false, true, false, false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = false, lowerCase = true, numbers = false, symbols = false)
     assert(a.exists(_.isLower))
   }
   test("T3"){
-    val a = new PassGenerator.Gen().generate(20,false, false, true, false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = false, lowerCase = false, numbers = true, symbols = false)
     assert(a.exists(_.isDigit))
   }
   test("T4"){
-    val a = new PassGenerator.Gen("-").generate(20,false, false, false, true)
+    val a = new PassGenerator.Gen("-").generate(20,upperCase = false, lowerCase = false, numbers = false, symbols = true)
     assert(a.contains("-"))
   }
   test("T5"){
-    val a = new PassGenerator.Gen().generate(20,true,true,false,false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = true,lowerCase = true,numbers = false,symbols = false)
     assert(a.exists(_.isLetter))
   }
   test("T6"){
-    val a = new PassGenerator.Gen().generate(20,true, false, true, false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = true, lowerCase = false, numbers = true, symbols = false)
     assert(a.exists(_.isLetterOrDigit))
   }
   test("T7"){
-    val a = new PassGenerator.Gen("$").generate(20,true, false, false, true)
+    val a = new PassGenerator.Gen("$").generate(20,upperCase = true, lowerCase = false, numbers = false, symbols = true)
     assert(a.contains("$"))
   }
   test("T8"){
-    val a = new PassGenerator.Gen().generate(20,false, true, true, false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = false, lowerCase = true, numbers = true, symbols = false)
     assert(a.exists(_.isLetterOrDigit))
   }
   test("T9"){
-    val a = new PassGenerator.Gen("*").generate(20,false, true, false ,true)
+    val a = new PassGenerator.Gen("*").generate(20,upperCase = false, lowerCase = true, numbers = false ,symbols = true)
     assert(a.contains("*"))
   }
   test("T10") {
-    val a = new PassGenerator.Gen("!").generate(20, false, false, true, true)
+    val a = new PassGenerator.Gen("!").generate(20, upperCase = false, lowerCase = false, numbers = true, symbols = true)
     assert(a.contains("!"))
   }
   test("T11"){
-    val a = new PassGenerator.Gen().generate(20,true,true,true,false)
+    val a = new PassGenerator.Gen().generate(20,upperCase = true,lowerCase = true,numbers = true,symbols = false)
     assert(a.exists(_.isLetterOrDigit))
   }
   test("T12"){
-    val a = new PassGenerator.Gen("@").generate(20,true,true,false, true)
+    val a = new PassGenerator.Gen("@").generate(20,upperCase = true,lowerCase = true,numbers = false, symbols = true)
     assert(a.contains("@"))
   }
   test("T13"){
-    val a = new PassGenerator.Gen(":").generate(20,true,false, true,true)
+    val a = new PassGenerator.Gen(":").generate(20,upperCase = true,lowerCase = false, numbers = true,symbols = true)
     assert(a.contains(":"))
   }
   test("T14"){
-    val a = new PassGenerator.Gen(":").generate(20,false,true, true,true)
+    val a = new PassGenerator.Gen(":").generate(20,upperCase = false,lowerCase = true, numbers = true,symbols = true)
     assert(a.contains(":"))
   }
   test("T15"){
-    val a = new PassGenerator.Gen(",").generate(20,true,true, true,true)
+    val a = new PassGenerator.Gen(",").generate(20,upperCase = true,lowerCase = true, numbers = true,symbols = true)
     assert(a.contains(","))
   }
 
